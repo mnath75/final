f49b6804c0ff11b430ce9f74f8849de260223e1a
mnath75 <mnath.snt@gmail.com>
2022-11-22 19:47:48 +0530

    this one first commit

--- gunicorn.sh
+++ gunicorn.sh
@@ -2,7 +2,7 @@
 
 source env/bin/activate
 
-cd /var/lib/jenkins/workspace/django-cicd/app
+cd /var/lib/jenkins/workspace/django-cicd/Application_Main
 
 python3 manage.py makemigrations
 python3 manage.py migrate
