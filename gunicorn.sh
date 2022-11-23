f1e69d420f6f12adc9b57250968482bfbfa655da
mnath75 <mnath.snt@gmail.com>
2022-11-23 10:09:52 +0530

    this one first commit

--- gunicorn.sh
+++ gunicorn.sh
@@ -2,7 +2,7 @@
 
 source env/bin/activate
 
-cd /var/lib/jenkins/workspace/django-cicd/Application_Main
+cd /var/lib/jenkins/workspace/final/Application_Main
 
 python3 manage.py makemigrations
 python3 manage.py migrate
@@ -10,7 +10,7 @@ python3 manage.py collectstatic -- no-input
 
 echo "Migrations done"
 
-cd /var/lib/jenkins/workspace/django-cicd
+cd /var/lib/jenkins/workspace/final
 
 sudo cp -rf gunicorn.socket /etc/systemd/system/
 sudo cp -rf gunicorn.service /etc/systemd/system/
