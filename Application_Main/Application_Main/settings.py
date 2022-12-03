5352871dff1170e4539b776d7846615a51b7e4c5
mnath75 <mnath.snt@gmail.com>
2022-12-03 22:16:46 +0530

    fourth day

--- Application_Main/Application_Main/settings.py
+++ Application_Main/Application_Main/settings.py
@@ -143,10 +143,14 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/report')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+
+]
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    
+    '/var/www/static/',
 ]
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, "assests")
