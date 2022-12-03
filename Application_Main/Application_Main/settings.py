55821ac75f3a3567558e537cf3329ab4ac6f1864
mnath75 <mnath.snt@gmail.com>
2022-12-03 21:41:28 +0530

    fourth day

--- Application_Main/Application_Main/settings.py
+++ Application_Main/Application_Main/settings.py
@@ -146,7 +146,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
+    
 ]
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, "assests")
