fce80598f34830341a91e27ce043a6e923a05981
mnath75 <mnath.snt@gmail.com>
2022-12-04 14:57:24 +0530

    six test

--- Application_Main/Application_Main/settings.py
+++ Application_Main/Application_Main/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = '/var/lib/jenkins/workspace/final'
 #STATIC_URL = '/assests/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/report')
